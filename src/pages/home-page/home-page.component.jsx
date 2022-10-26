77963429d99fa56e8d8fbc431d11267e93b9c0b9
n4-t3 <nathanmekuria@gmail.com>
2022-10-26 21:13:54 +0900

    converted class based component to function based component

--- src/pages/home-page/home-page.component.jsx
+++ src/pages/home-page/home-page.component.jsx
@@ -1,34 +1,26 @@
-import React from 'react'
 import './home-page.component.scss'
 import HomeCard from '../../components/home-cards/home-card.component'
 import ShoppingPage from '../shopping-page/shopping-page.component'
-import data from '../../data/data'
 import { BrowserRouter as Router, Switch, Route, withRouter, Link } from 'react-router-dom'
+import React, { useState } from 'react'
 
-class HomePage extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            data
-        }
-    }
+const HomePage = (props)=>{
+    const info = [{ 'id': 1, 'item': 'hat', 'imgSrc': 'https://source.unsplash.com/6NVrH0HB_DE', 'closet': [{ 'id': 1, 'name': 'cow boy hat', 'imgSrc': 'https://source.unsplash.com/BF_Vxk1rHX4', 'price': 25 }, { 'id': 2, 'name': 'oldie hat', 'imgSrc': 'https://source.unsplash.com/xYRM6tusURI', 'price': 20 }, { 'id': 3, 'name': 'party hat', 'imgSrc': 'https://source.unsplash.com/6NVrH0HB_DE', 'price': 15 }, { 'id': 4, 'name': 'all star shoes', 'imgSrc': 'https://source.unsplash.com/mWYhrOiAgmA', 'price': 50 }, { 'id': 5, 'name': 'pet hat', 'imgSrc': 'https://source.unsplash.com/AQRp2NH-O8k', 'price': 10 }]}, { 'id': 2, 'item': 'shoes', 'imgSrc': 'https://source.unsplash.com/164_6wVEHfI','closet': [{ 'id': 1, 'name': 'vans shoes', 'imgSrc': 'https://source.unsplash.com/a-QH9MAAVNI', 'price': 40 }, { 'id': 2, 'name': 'Jordan shoes', 'imgSrc': 'https://source.unsplash.com/_fOL6ebfECQ', 'price': 150 }, { 'id': 3, 'name': 'heel shoes', 'imgSrc': 'https://source.unsplash.com/E-0ON3VGrBc', 'price': 120 }, { 'id': 4, 'name': 'nike shoes', 'imgSrc': 'https://source.unsplash.com/NOpsC3nWTzY', 'price': 200 }, { 'id': 5, 'name': 'nike trainers', 'imgSrc': 'https://source.unsplash.com/164_6wVEHfI', 'price': 110 }] }, { 'id': 3, 'item': 'glasses', 'imgSrc': 'https://source.unsplash.com/bSjqyqukCjY' ,'closet': [{ 'id': 1, 'name': 'reading glasses', 'imgSrc': 'https://source.unsplash.com/aVvZJC0ynBQ', 'price': 70 },{ 'id': 2, 'name': 'sun glasses', 'imgSrc': 'https://source.unsplash.com/K62u25Jk6vo', 'price': 150 }, { 'id': 3, 'name': 'blue light glasses', 'imgSrc': 'https://source.unsplash.com/LHaqQkG_mgQ', 'price': 100 }, { 'id': 4, 'name': 'stylish glasses', 'imgSrc': 'https://source.unsplash.com/BBEbl2WKRlU', 'price': 50 }, { 'id': 5, 'name': 'Tea shades', 'imgSrc': 'https://source.unsplash.com/QNAKpUEFJkQ', 'price': 60 }] }, { 'id': 4, 'item': 'Men', 'imgSrc': 'https://source.unsplash.com/z3otWAqSs-g','closet': [{ 'id': 1, 'name': 'suit', 'imgSrc': 'https://source.unsplash.com/h5cd51KXmRQ', 'price': 200 },{ 'id': 2, 'name': 'hoodie', 'imgSrc': 'https://source.unsplash.com/LI7jB1925j0', 'price': 30 }, { 'id': 3, 'name': 'jacket', 'imgSrc': 'https://source.unsplash.com/CSh-hNHc3Y8', 'price': 70 },{ 'id': 4, 'name': 'T-shirt', 'imgSrc': 'https://source.unsplash.com/sYhUhse5uT8', 'price': 20 }, { 'id': 5, 'name': 'Trousers', 'imgSrc': 'https://source.unsplash.com/_M6gy9oHgII', 'price': 60 }]}, { 'id': 5, 'item': 'Women', 'imgSrc': 'https://source.unsplash.com/zaeBgN8OTsg','closet': [{ 'id': 1, 'name': 'jacket', 'imgSrc': 'https://source.unsplash.com/eAfrl7A6gBs', 'price': 50 },{ 'id': 2, 'name': 'T-shirt', 'imgSrc': 'https://source.unsplash.com/3U9BCWHMhUw', 'price': 30 }, { 'id': 3, 'name': 'Trouser', 'imgSrc': 'https://source.unsplash.com/A77FAov-OzI', 'price': 70 },{ 'id': 4, 'name': 'Dress', 'imgSrc': 'https://source.unsplash.com/G9XMLUAjETM', 'price': 40 }, { 'id': 5, 'name': 'Blouse', 'imgSrc': 'https://source.unsplash.com/MWWKA47KemU', 'price': 60 }]}]
 
-    render() {
-        return (
-            <div className="card-container">
-                {this.state.data.map(element => {
-                    return (
-                        <HomeCard data={element} key={element.id} />
-                    )
-                }
-                )}
-            </div>
+    const [data,setData] = useState(info)
 
-            // < ShoppingPage data={this.state.data[0]} />
-        )
-    }
+    return (
+        <div className="card-container">
+            {data.map(element => {
+                return (
+                    <HomeCard data={element} key={element.id} />
+                )
+            }
+            )}
+        </div>
+
+        // < ShoppingPage data={this.state.data[0]} />
+    )
 }
-
-
 
 export default HomePage
